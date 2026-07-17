6868c93896cee80922c17e22e97b7ede5e4c8684
andrewnoyes <andrew@noyes.io>
2026-07-17 09:24:48 -0600

    collapse xp to accordion on mobile

--- components/sections/work-experience.tsx
+++ components/sections/work-experience.tsx
@@ -1,76 +1,83 @@
-import {
-  Badge,
-  Container,
-  Group,
-  Tabs,
-  Text,
-  useMantineTheme,
-} from '@mantine/core';
+import { Accordion, Badge, Container, Group, Tabs, Text } from '@mantine/core';
 import { useIsMobile } from '../../hooks';
-import { jobDescriptions, slugify } from '../../utils';
+import { JobDescription, jobDescriptions, slugify } from '../../utils';
 import { SectionHeader } from './section-header';
 import { WorkExperienceItem } from './work-experience-item';
 
+const JobCompany = ({ job }: { job: JobDescription }) => {
+  return (
+    <Group noWrap>
+      <Text
+        sx={{
+          fontFamily: 'monospace',
+          fontSize: 'inherit',
+        }}
+      >
+        {job.company}
+      </Text>
+      {job.active && (
+        <Badge
+          color="green"
+          variant="dot"
+          radius="xs"
+          sx={{
+            fontFamily: 'monospace',
+            textTransform: 'lowercase',
+          }}
+        >
+          active
+        </Badge>
+      )}
+    </Group>
+  );
+};
+
 export const WorkExperience = () => {
-  const theme = useMantineTheme();
   const isMobile = useIsMobile();
 
   const defaultWorkTab = slugify(jobDescriptions[0].company);
+
+  const renderContent = () => {
+    if (isMobile) {
+      return (
+        <Accordion defaultValue={defaultWorkTab}>
+          {jobDescriptions.map((job) => (
+            <Accordion.Item key={job.company} value={slugify(job.company)}>
+              <Accordion.Control>
+                <JobCompany job={job} />
+              </Accordion.Control>
+              <Accordion.Panel>
+                <WorkExperienceItem description={job} />
+              </Accordion.Panel>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      );
+    }
+
+    return (
+      <Tabs defaultValue={defaultWorkTab} orientation="vertical">
+        <Tabs.List>
+          {jobDescriptions.map((job) => (
+            <Tabs.Tab key={job.company} value={slugify(job.company)}>
+              <JobCompany job={job} />
+            </Tabs.Tab>
+          ))}
+        </Tabs.List>
+        {jobDescriptions.map((job) => (
+          <Tabs.Panel key={job.company} value={slugify(job.company)} pl="xl">
+            <WorkExperienceItem description={job} />
+          </Tabs.Panel>
+        ))}
+      </Tabs>
+    );
+  };
 
   return (
     <section id="experience">
       <Container size="md" sx={{ paddingTop: '10%', paddingBottom: '10%' }}>
         <SectionHeader title="Work experience" />
-        <Tabs
-          defaultValue={defaultWorkTab}
-          orientation={isMobile ? 'horizontal' : 'vertical'}
-          styles={{
-            tab: {
-              padding: isMobile ? theme.spacing.sm : undefined,
-            },
-          }}
-        >
-          <Tabs.List>
-            {jobDescriptions.map((job) => (
-              <Tabs.Tab key={job.company} value={slugify(job.company)}>
-                <Group spacing={isMobile ? 'sm' : 'xl'}>
-                  <Text
-                    sx={{
-                      fontFamily: 'monospace',
-                      fontSize: isMobile ? 'medium' : 'inherit',
-                    }}
-                  >
-                    {job.company}
-                  </Text>
-                  {job.active && (
-                    <Badge
-                      color="green"
-                      variant="dot"
-                      radius="xs"
-                      size={isMobile ? 'sm' : undefined}
-                      sx={{
-                        fontFamily: 'monospace',
-                        textTransform: 'lowercase',
-                      }}
-                    >
-                      active
-                    </Badge>
-                  )}
-                </Group>
-              </Tabs.Tab>
-            ))}
-          </Tabs.List>
-          {jobDescriptions.map((job) => (
-            <Tabs.Panel
-              key={job.company}
-              value={slugify(job.company)}
-              pl={isMobile ? 0 : 'xl'}
-              pt={isMobile ? 'xl' : 0}
-            >
-              <WorkExperienceItem description={job} />
-            </Tabs.Panel>
-          ))}
-        </Tabs>
+        {renderContent()}
       </Container>
     </section>
   );
