302ba7f13c9552af7589e84865a791471817ae58
andrewnoyes <andrew@noyes.io>
2026-07-25 13:11:47 -0600

    copdb badge

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -9,7 +9,10 @@ import {
   Title,
 } from '@mantine/core';
 import Link from 'next/link';
+import { ExternalLink } from '../external-link';
 import { PrideFlagPicker } from '../pride-flag-picker';
+
+const BADGE_WIDTH = 150;
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -47,11 +50,19 @@ const useStyles = createStyles((theme) => ({
   },
   badge: {
     border: '1px solid',
-    borderColor: theme.colors.yellow,
+    borderColor: theme.colors.violet,
     paddingLeft: theme.spacing.xs,
     paddingRight: theme.spacing.xs,
     borderRadius: theme.radius.sm,
     height: 50,
+    width: BADGE_WIDTH,
+  },
+  internalLink: {
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
   },
 }));
 
@@ -73,24 +84,42 @@ export const Hero = () => {
           <Link href="mailto:andrew@noyes.io">
             <Button
               variant="gradient"
+              sx={{ width: BADGE_WIDTH }}
               gradient={{ from: 'grape', to: 'violet' }}
               size="lg"
             >
               Email me!
             </Button>
           </Link>
-          <Group spacing="xs" noWrap className={classes.badge}>
-            <Image
-              src="/thinksies.png"
-              height={32}
-              width={32}
-              alt="thinksies"
-            />
-            <Text size="xs" sx={{ lineHeight: 1.3 }}>
-              made by a human <br />
-              <strong>not by ai</strong>
-            </Text>
-          </Group>
+
+          <ExternalLink href="https://app.copdb.org" sx={{ color: 'inherit' }}>
+            <Group spacing="xs" noWrap className={classes.badge}>
+              <Image
+                src="/pig-glasses.png"
+                height={32}
+                width={32}
+                alt="cop pig with sunglasses."
+              />
+              <Text size="xs" sx={{ lineHeight: 1.3 }}>
+                report cops at <br />
+                <strong>copdb.org</strong>
+              </Text>
+            </Group>
+          </ExternalLink>
+          <Link href="/notes/_tech-critique" className={classes.internalLink}>
+            <Group spacing="xs" noWrap className={classes.badge}>
+              <Image
+                src="/thinksies.png"
+                height={32}
+                width={32}
+                alt="thinksies"
+              />
+              <Text size="xs" sx={{ lineHeight: 1.3 }}>
+                made by human <br />
+                <strong>not by ai</strong>
+              </Text>
+            </Group>
+          </Link>
         </Group>
       </Container>
     </Container>
