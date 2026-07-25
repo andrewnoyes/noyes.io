2c2173091642f17edab459653dc20bc55dafe47e
andrewnoyes <andrew@noyes.io>
2026-07-25 12:51:01 -0600

    move to own component

---
+++ components/pride-flag-picker.tsx
@@ -0,0 +1,60 @@
+import { ActionIcon, Box, Group, Tooltip } from '@mantine/core';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
+import { useState } from 'react';
+import { FlagVariant, PrideFlag } from './pride-flag';
+
+const ALL_VARIANTS = Object.values(FlagVariant);
+
+export const PrideFlagPicker = () => {
+  const [variant, setVariant] = useState<FlagVariant>(FlagVariant.Rainbow);
+
+  const currentIndex = ALL_VARIANTS.indexOf(variant);
+  const lastIndex = ALL_VARIANTS.length - 1;
+
+  const nextVariant =
+    currentIndex === lastIndex
+      ? ALL_VARIANTS[0]
+      : ALL_VARIANTS[currentIndex + 1];
+
+  const previousVariant =
+    currentIndex === 0
+      ? ALL_VARIANTS[lastIndex]
+      : ALL_VARIANTS[currentIndex - 1];
+
+  const previousLabel = `${previousVariant} pride flag`;
+  const nextLabel = `${nextVariant} pride flag`;
+
+  const handleNextFlag = () => {
+    setVariant(nextVariant);
+  };
+
+  const handlePreviousFlag = () => {
+    setVariant(previousVariant);
+  };
+
+  return (
+    <Group align="center" position="center" spacing="xl" noWrap>
+      <ActionIcon
+        aria-label={previousLabel}
+        title={previousLabel}
+        onClick={handlePreviousFlag}
+        size="sm"
+      >
+        <IconChevronLeft />
+      </ActionIcon>
+      <Tooltip label={`${variant} pride flag`}>
+        <Box>
+          <PrideFlag width={150} variant={variant} />
+        </Box>
+      </Tooltip>
+      <ActionIcon
+        aria-label={nextLabel}
+        title={nextLabel}
+        onClick={handleNextFlag}
+        size="sm"
+      >
+        <IconChevronRight />
+      </ActionIcon>
+    </Group>
+  );
+};
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,6 +1,4 @@
 import {
-  ActionIcon,
-  Box,
   Button,
   Container,
   createStyles,
@@ -9,12 +7,9 @@ import {
   Space,
   Text,
   Title,
-  Tooltip,
 } from '@mantine/core';
-import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 import Link from 'next/link';
-import { useState } from 'react';
-import { FlagVariant, PrideFlag } from '../pride-flag';
+import { PrideFlagPicker } from '../pride-flag-picker';
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -60,61 +55,12 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const ALL_VARIANTS = Object.values(FlagVariant);
-
 export const Hero = () => {
   const { classes } = useStyles();
-  const [variant, setVariant] = useState<FlagVariant>(FlagVariant.Rainbow);
-
-  const currentIndex = ALL_VARIANTS.indexOf(variant);
-  const lastIndex = ALL_VARIANTS.length - 1;
-
-  const nextVariant =
-    currentIndex === lastIndex
-      ? ALL_VARIANTS[0]
-      : ALL_VARIANTS[currentIndex + 1];
-
-  const previousVariant =
-    currentIndex === 0
-      ? ALL_VARIANTS[lastIndex]
-      : ALL_VARIANTS[currentIndex - 1];
-
-  const previousLabel = `${previousVariant} pride flag`;
-  const nextLabel = `${nextVariant} pride flag`;
-
-  const handleNextFlag = () => {
-    setVariant(nextVariant);
-  };
-
-  const handlePreviousFlag = () => {
-    setVariant(previousVariant);
-  };
 
   return (
     <Container className={classes.wrapper}>
-      <Group align="center" position="center" spacing="xl" noWrap>
-        <ActionIcon
-          aria-label={previousLabel}
-          title={previousLabel}
-          onClick={handlePreviousFlag}
-          size="sm"
-        >
-          <IconChevronLeft />
-        </ActionIcon>
-        <Tooltip label={`${variant} pride flag`}>
-          <Box>
-            <PrideFlag width={150} variant={variant} />
-          </Box>
-        </Tooltip>
-        <ActionIcon
-          aria-label={nextLabel}
-          title={nextLabel}
-          onClick={handleNextFlag}
-          size="sm"
-        >
-          <IconChevronRight />
-        </ActionIcon>
-      </Group>
+      <PrideFlagPicker />
       <Container p={0} size={650} className={classes.greetingContainer}>
         <Text className={classes.greeting}>Hey! My name is</Text>
         <Title className={classes.title}>Andrew Noyes.</Title>
