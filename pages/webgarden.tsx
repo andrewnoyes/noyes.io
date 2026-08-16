6052d97ad36de7561e91aee45837cdea4f1312de
andrewnoyes <andrew@noyes.io>
2026-08-16 12:43:43 -0600

    add updated at & v13.12.0 to webgarden plant

--- pages/webgarden.tsx
+++ pages/webgarden.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from '@mantine/core';
+import { Paper, Stack, Text } from '@mantine/core';
 import { CopDbBadgeTag, ExternalLink } from '../components';
 import { BadgeTag } from '../components/badge-tags/badge-tag';
 import { PrideFlagPicker } from '../components/pride-flag-picker';
@@ -21,6 +21,8 @@ const MeBadgeTag = () => (
   </ExternalLink>
 );
 
+const LAST_UPDATED = new Date('08/16/2026');
+
 export default function Webgarden() {
   return (
     <Stack
@@ -41,8 +43,26 @@ export default function Webgarden() {
       <Paper
         withBorder
         p="sm"
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          position: 'relative',
+        }}
       >
+        <Text
+          sx={{
+            bottom: 14,
+            left: 6,
+            position: 'absolute',
+            textOrientation: 'mixed',
+            writingMode: 'vertical-rl',
+          }}
+          size={11}
+          c="dimmed"
+        >
+          v13.12.0 • {LAST_UPDATED.toLocaleDateString()}
+        </Text>
         <PrideFlagPicker width={100} />
         <Stack spacing="xs" mt="lg">
           <MeBadgeTag />
