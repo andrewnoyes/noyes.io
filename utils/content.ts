3d2e29757c4c59a93cb610e2a8b1716523595dc6
andrewnoyes <andrew@noyes.io>
2026-08-30 13:52:17 -0600

    more words

--- utils/content.ts
+++ utils/content.ts
@@ -10,7 +10,7 @@ export const jobDescriptions: JobDescription[] = [
       'Architected the Nexus API and frontend for store connectivity monitoring, software release management, and invoicing utilities. Structured as a “modular monolith”, each bounded by their domain with intermodule communication using a local message bus.',
       'Established the new Hive API service that runs in stores and talks to Nexus. Hive powers the new task management and communication log, rackhealth and backstock, and the overhauled payments processing features.',
       `Currently building the new store kiosk check-in app! All customers interact with this app during their checkout, allowing them to edit their details, opt-in to marketing, sign the vendor contract, and view their points. On check-in, an event is then sent to the appropriate register in real-time, ensuring a clean hand-off.`,
-      `Alongside this new app, I built our UI component library that is compatible with React and React Native. This powers all the UI in the new check-in experience, and is "concept aware", adjusting its theme to match the appropriate brand (Uptown Cheapskate vs Kid to Kid).`,
+      `Alongside this new app, I built our UI component library that is compatible with React and React Native. This powers all the UI in the new check-in experience, and is concept aware, adjusting its theme to match the appropriate brand (e.g. Uptown Cheapskate, Kid to Kid).`,
     ],
     techStack: [
       'ASP.NET Core',
